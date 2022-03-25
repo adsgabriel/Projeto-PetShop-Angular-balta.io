2c3f90a1bc5f0150fa88b1751619018c050910aa
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2022-03-24 22:31:26 -0300

    add login service token

--- src/app/pages/account/login-page/login-page.component.ts
+++ src/app/pages/account/login-page/login-page.component.ts
@@ -4,7 +4,8 @@ import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-login-page',
-  templateUrl: './login-page.component.html'
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
   public form: FormGroup;
@@ -29,6 +30,33 @@ export class LoginPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const token = localStorage.getItem('petshop.token');
+      if(token){
+        this
+        .service
+        .authenticate(this.form.value)
+        .subscribe(
+          (data: any) => {
+            localStorage.setItem('petshop.token', data.token)
+          },
+          (err) => {
+            localStorage.clear();
+          }
+        );
+      }
   }
 
+  submit(){
+    this
+    .service
+    .authenticate(this.form.value)
+    .subscribe(
+      (data: any) => {
+        localStorage.setItem('petshop.token', data.token)
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
