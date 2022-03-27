fdbd2810e1ff91ac4ad1067151a9bf45b6d84f85
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2022-03-27 18:05:06 -0300

    add cart util

--- src/app/pages/store/cart-page/cart-page.component.ts
+++ src/app/pages/store/cart-page/cart-page.component.ts
@@ -1,14 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
+import { CartUtil } from 'src/app/utils/cart.util';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html'
 })
 export class CartPageComponent implements OnInit {
+  public cart: Cart = new Cart()
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loadCart();
   }
 
+  public loadCart() {
+    this.cart = CartUtil.get();
+  }
+
+  public remove(item: any) {
+    let index = this.cart.items.indexOf(item);
+    this.cart.items.splice(index, 1);
+    CartUtil.update(this.cart);
+  }
+
+  public clear(){
+    CartUtil.clear();
+    this.loadCart();
+  }
+
+  public total(){
+    let total = 0;
+    this.cart.items.forEach((item) => {
+      total += (item.price * item.quantity);
+    });
+    return total;
+  }
 }
